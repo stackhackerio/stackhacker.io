2565ef2a931cc889926f86f3b8fb88f1ff6aa8e9
Hirotaka Mizutani <hirotaka@mizutani.to>
2021-06-28 17:35:00 +0900

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,32 @@ import 'tailwindcss/tailwind.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/source-code-pro/400.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
+
 import { DefaultSeo } from 'next-seo'
 import SEO from '../../next-seo.config'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <DefaultSeo {...SEO} />
