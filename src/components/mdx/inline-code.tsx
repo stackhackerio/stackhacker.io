d75d7e86d7767000751f09736eaf905c6c370883
Hirotaka Mizutani <hirotaka@mizutani.to>
2021-07-06 15:13:51 +0900

    Add Classless CSS post

--- src/components/mdx/inline-code.tsx
+++ src/components/mdx/inline-code.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function InlineCode({ codeString }: Props) {
   return (
-    <span className="font-code text-base py-0.5 px-2 bg-gray-100 rounded">
+    <span className="font-code text-base py-0.5 px-2 mx-1 bg-gray-100 rounded">
       {codeString}
     </span>
   )
